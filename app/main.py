96ae3ced13082410a93f1cb19ebf0f91982ee866
Rahul Gupta <coolrahulb1995@gmail.com>
2025-06-10 23:39:52 +0530

    connect to db and check wrote a dummy get api to verify

--- app/main.py
+++ app/main.py
@@ -1,9 +1,25 @@
 # main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+from app.db.session import SessionLocal
 
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def home():
     return {"message": "Welcome to Home page"}
+
+@app.get("/dummy/")
+def dummy_fun(db: Session = Depends(get_db)):
+    result = db.execute(text("SELECT * FROM dummy;"))
+    rows = result.fetchall()
+    return [dict(row._mapping) for row in rows]
