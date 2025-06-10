9acde77a7e7c71ad0d31026cd2a8195e1d7bd704
Rahul Gupta <coolrahulb1995@gmail.com>
2025-06-10 23:49:57 +0530

    setup env file

--- app/db/session.py
+++ app/db/session.py
@@ -2,8 +2,11 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5433/recipe"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
