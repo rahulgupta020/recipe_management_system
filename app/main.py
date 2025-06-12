d03dd428c28cc16e27a522107a235db301aff14b
Rahul Gupta <coolrahulb1995@gmail.com>
2025-06-12 23:18:31 +0530

    implement auth jwt register login

--- app/main.py
+++ app/main.py
@@ -1,25 +1,39 @@
 # main.py
 
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from sqlalchemy import text
-from app.db.session import SessionLocal
+from fastapi import FastAPI
+from app.routes import auth
 
 app = FastAPI()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+app.include_router(auth.router)
 
-@app.get("/")
-def home():
-    return {"message": "Welcome to Home page"}
 
-@app.get("/dummy/")
-def dummy_fun(db: Session = Depends(get_db)):
-    result = db.execute(text("SELECT * FROM dummy;"))
-    rows = result.fetchall()
-    return [dict(row._mapping) for row in rows]
+
+
+
+
+
+
+# from fastapi import FastAPI, Depends
+# from sqlalchemy.orm import Session
+# from sqlalchemy import text
+# from app.db.session import SessionLocal
+
+# app = FastAPI()
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+# @app.get("/")
+# def home():
+#     return {"message": "Welcome to Home page"}
+
+# @app.get("/dummy/")
+# def dummy_fun(db: Session = Depends(get_db)):
+#     result = db.execute(text("SELECT * FROM dummy;"))
+#     rows = result.fetchall()
+#     return [dict(row._mapping) for row in rows]
