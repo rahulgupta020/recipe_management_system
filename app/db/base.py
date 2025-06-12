b78a2704a88c73bb316786d4e9316cb250e5e16b
Rahul Gupta <coolrahulb1995@gmail.com>
2025-06-12 23:34:06 +0530

    add all models

--- app/db/base.py
+++ app/db/base.py
@@ -1,4 +1,6 @@
 # db/base.py
 
 from app.db.base_class import Base
-from app.models import user_model  # Add other models here as needed
+from app.models.user_model import User
+from app.models.recipe_model import Recipe
+from app.models.category_model import Category
